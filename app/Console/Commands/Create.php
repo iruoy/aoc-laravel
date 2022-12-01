4d4660e336e7504b3497a7c5c8f7023ed3bd6f7f
Youri Wijnands <youri@wijnands.me>
2022-12-01 22:54:39 +0100

    Add commands

---
+++ app/Console/Commands/Create.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Create extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create {year?} {day?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $now = now();
+
+        $year = $this->argument('year') ?? $now->format('Y');
+        $day = $this->argument('day') ?? $now->format('d');
+
+        $puzzle = __DIR__.'/../../../app/Puzzles/Year'.$year.'/Day'.$day.'.php';
+        if (!file_exists($puzzle)) {
+            file_put_contents($puzzle, <<<END
+<?php
+
+declare(strict_types=1);
+
+namespace App\Puzzles\Year$year;
+
+use App\Puzzles\Puzzle;
+
+class Day$day extends Puzzle
+{
+    public function part1(string \$input): int
+    {
+        return 0;
+    }
+
+    public function part2(string \$input): int
+    {
+        return 0;
+    }
+}
+
+END
+);
+        }
+
+        $test = __DIR__.'/../../../tests/Unit/Year'.$year.'/Day'.$day.'Test.php';
+        if (!file_exists($test)) {
+            file_put_contents($test, <<<END
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Year$year;
+
+use Tests\Unit\DayTestCase;
+
+class Day{$day}Test extends DayTestCase
+{
+    public function testPart1(): void
+    {
+        \$this->assertSame(0, \$this->puzzle->part1(\$this->input));
+    }
+
+    public function testPart2(): void
+    {
+        \$this->assertSame(0, \$this->puzzle->part2(\$this->input));
+    }
+}
+
+END
+);
+        }
+
+        $data = __DIR__.'/../../../public/data/'.$year.'/'.$day;
+        if (!file_exists($data)) {
+            mkdir($data);
+        }
+
+        $example = __DIR__.'/../../../public/data/'.$year.'/'.$day.'/example.txt';
+        if (!file_exists($example)) {
+            file_put_contents($example, '');
+        }
+
+        $input = __DIR__.'/../../../public/data/'.$year.'/'.$day.'/input.txt';
+        if (!file_exists($input)) {
+            file_put_contents($input, '');
+        }
+
+        $this->info('All files are created.');
+
+        return Command::SUCCESS;
+    }
+}
---
+++ app/Console/Commands/Run.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Puzzles\Puzzle;
+use Illuminate\Console\Command;
+
+class Run extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:run {year?} {day?} {--test}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $now = now();
+
+        $year = $this->argument('year') ?? $now->format('Y');
+        $day = $this->argument('day') ?? $now->format('d');
+        $test = $this->option('test');
+
+        $classString = '\App\Puzzles\Year'.$year.'\Day'.$day;
+        if (!class_exists($classString)) {
+            $this->error('The solution for this day doesn\'t exist.');
+
+            return Command::FAILURE;
+        }
+
+        $solution = new $classString();
+        if (!$solution instanceof Puzzle) {
+            $this->error('The solution for this day doesn\'t exist.');
+
+            return Command::FAILURE;
+        }
+
+        $filename = __DIR__.'/../../../public/data/'.$year.'/'.$day.'/'.($test ? 'example' : 'input').'.txt';
+        if (!file_exists($filename)) {
+            $this->error('The '.($test ? 'example' : 'input').' for this day doesn\'t exist.');
+
+            return Command::FAILURE;
+        }
+
+        $data = file_get_contents($filename);
+        if (!is_string($data)) {
+            $this->error('The '.($test ? 'example' : 'input').' for this day doesn\'t exist.');
+
+            return Command::FAILURE;
+        }
+
+        $this->info('Part 1: '.$solution->part1($data));
+        $this->info('Part 2: '.$solution->part2($data));
+
+        return Command::SUCCESS;
+    }
+}
